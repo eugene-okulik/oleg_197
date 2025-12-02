fe3d4e2f7da0456e22c9c2e82ad0a3b27aa8a25a
Oleg Vargin <oleg_197@bk.ru>
2025-12-02 17:08:38 +0300

    Добавлены задачи по обработке чисел

---
+++ homework/oleg_vargin/Lesson_3/step_1.py
@@ -0,0 +1,6 @@
+a = 150
+b = 30
+
+print(f'{a} + {b} = {a + b}')
+print(f'{a} - {b} = {a - b}')
+print(f'{a} * {b} = {a * b}')
---
+++ homework/oleg_vargin/Lesson_3/step_3.py
@@ -0,0 +1,7 @@
+import math
+
+num_1 = 25
+num_2 = 94
+
+print(f'Среднее арифметическое: {(num_1 + num_2) / 2:.2f}')
+print(f'Среднее геометрическое: {math.sqrt(num_1 * num_2):.2f}')
