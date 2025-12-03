74962b9e0b391162d4965588d05f660b21784ffc
Oleg Vargin <oleg_197@bk.ru>
2025-12-03 18:04:13 +0300

    Исправления ошибок flake8

--- homework/oleg_vargin/Lesson_4/step_1.py
+++ homework/oleg_vargin/Lesson_4/step_1.py
@@ -1,8 +1,8 @@
 my_dict = {
-    'tuple' : (56, 2, False, '4', 5),
-    'list' : ['text', 50.2, 531, 21, True],
-    'dict' : {'Name' : 'Oliver', 'Second Name' : 'Brown' ,'Age' : 13, 'Height' : 143, 'Sex' : 'male '},
-    'set' : {None, 'text', 39, 15, 'last'}
+    'tuple': (56, 2, False, '4', 5),
+    'list': ['text', 50.2, 531, 21, True],
+    'dict': {'Name': 'Oliver', 'Second Name': 'Brown' ,'Age': 13, 'Height': 143, 'Sex': 'male'},
+    'set': {None, 'text', 39, 15, 'last'}
     }
 
 print(my_dict['tuple'][-1])
