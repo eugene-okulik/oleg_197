b9c695775481c7f1027e001016fdc1290ce017bc
Oleg Vargin <oleg_197@bk.ru>
2025-12-03 18:13:03 +0300

    вторая попытка исправления ошибок flake8

--- homework/oleg_vargin/Lesson_4/step_1.py
+++ homework/oleg_vargin/Lesson_4/step_1.py
@@ -1,7 +1,13 @@
 my_dict = {
     'tuple': (56, 2, False, '4', 5),
     'list': ['text', 50.2, 531, 21, True],
-    'dict': {'Name': 'Oliver', 'Second Name': 'Brown' ,'Age': 13, 'Height': 143, 'Sex': 'male'},
+    'dict': {
+        'Name': 'Oliver',
+        'Second Name': 'Brown',
+        'Age': 13,
+        'Height': 143,
+        'Sex': 'male'
+    },
     'set': {None, 'text', 39, 15, 'last'}
     }
 
@@ -13,7 +19,7 @@ print(my_dict['list'])
 my_dict['list'].pop(1)
 print(my_dict['list'])
 
-my_dict['dict']['i am a tuple'] = 'some value'
+my_dict['dict'][('i am a tuple',)] = 'some value'
 print(my_dict['dict'])
 
 my_dict['dict'].pop('Sex')
