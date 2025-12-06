2c7d6a37f1bef44e36a74b34d80be125866324fc
Oleg Vargin <oleg_197@bk.ru>
2025-12-06 15:27:15 +0300

    Исправления flake8

--- homework/oleg_vargin/Lesson_8/step_1.py
+++ homework/oleg_vargin/Lesson_8/step_1.py
@@ -4,8 +4,8 @@ salary = int(input("Enter your salary: "))
 bonus = random.choice([True, False])
 
 if bonus:
-     random_bonus = random.randint(0, 10000)
-     total = salary + random_bonus
-     print(f"{salary}, {bonus} - '${salary + random_bonus}'")
+	random_bonus = random.randint(0, 10000)
+	total = salary + random_bonus
+	print(f"{salary}, {bonus} - '${salary + random_bonus}'")
 else:
-     print(f"{salary}, {bonus} - '${salary}'")
+	print(f"{salary}, {bonus} - '${salary}'")
--- homework/oleg_vargin/Lesson_8/step_2.py
+++ homework/oleg_vargin/Lesson_8/step_2.py
@@ -4,24 +4,25 @@ sys.set_int_max_str_digits(30000)
 
 
 def fibonacci_generator():
-    a, b = 0, 1
-    while True:
-        yield a
-        a, b = b, a + b
+	a, b = 0, 1
+	while True:
+		yield a
+		a, b = b, a + b
+
 
 num_5 = num_200 = num_1000 = num_100000 = None
 
 fib = fibonacci_generator()
 for i in range(100000):
-    current = next(fib)
-    if i == 4:
-        num_5 = current
-    elif i == 199:
-        num_200 = current
-    elif i == 999:
-        num_1000 = current
-    elif i == 99999:
-        num_100000 = current
+	current = next(fib)
+	if i == 4:
+		num_5 = current
+	elif i == 199:
+		num_200 = current
+	elif i == 999:
+		num_1000 = current
+	elif i == 99999:
+		num_100000 = current
 
 print(f"5-е число Фибоначчи: {num_5}")
 print(f"200-е число Фибоначчи: {num_200}")
