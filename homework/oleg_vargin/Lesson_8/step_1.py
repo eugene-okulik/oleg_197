4c5e410e09b8d28dc2f6fb97c179ad825d887fa0
Oleg Vargin <oleg_197@bk.ru>
2025-12-06 15:18:05 +0300

    Добавил задание к 8 уроку и виртуальное окружение

---
+++ homework/oleg_vargin/Lesson_8/step_1.py
@@ -0,0 +1,11 @@
+import random
+
+salary = int(input("Enter your salary: "))
+bonus = random.choice([True, False])
+
+if bonus:
+     random_bonus = random.randint(0, 10000)
+     total = salary + random_bonus
+     print(f"{salary}, {bonus} - '${salary + random_bonus}'")
+else:
+     print(f"{salary}, {bonus} - '${salary}'")
