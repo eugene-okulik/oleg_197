15f2dbe7566a5bbf3ad9e45d7de7ecc886d3f335
Oleg Vargin <oleg_197@bk.ru>
2025-12-28 10:45:40 +0300

    Исправления домашнего задания 20

--- homework/oleg_vargin/Lesson_20/test_rabota_s_api.py
+++ homework/oleg_vargin/Lesson_20/test_rabota_s_api.py
@@ -10,6 +10,31 @@ base_url = 'http://objapi.course.qa-practice.com/'
 size = ['XS', 'S', 'M', 'L', 'XL', 'AVERAGE', 'UNBELIEVABLE']
 
 
+def post():
+    body = {
+        'name': fake.name(),
+        'data': {
+            'color': fake.color_name(),
+            'size': random.choice(size)
+        }
+    }
+    post_url = base_url + 'object'
+    response = requests.post(post_url, json=body)
+    assert response.status_code == 200
+    print(f'Объект создан: {response.text}')
+    post_id = response.json()['id']
+    return post_id
+
+
+def delete(post_id):
+    delete_url = f'{base_url}object/{post_id}'
+    response = requests.delete(delete_url)
+    if response.status_code == 200:
+        print(f'Объект {post_id} удален')
+    else:
+        print(f'Warning: не удалось удалить объект {post_id}')
+
+
 @pytest.fixture(scope='session', autouse=True)
 def session_setup_teardown():
     print('\nStart testing')
@@ -26,25 +51,13 @@ def function_setup_teardown():
 
 @pytest.fixture()
 def post_and_delete():
-    body = {
-        'name': fake.name(),
-        'data': {
-            'color': fake.color_name(),
-            'size': random.choice(size)
-        }
-    }
-    post_url = base_url + 'object'
-    response = requests.post(post_url, json=body)
-    assert response.status_code == 200
-    print(f'Объект создан: {response.text}')
-    post_id = response.json()['id']
-    yield post_id
-    delete_url = f'{base_url}object/{post_id}'
-    response = requests.delete(delete_url)
-    if response.status_code == 200:
-        print(f'Объект {post_id} удален')
-    else:
-        print(f'Warning: не удалось удалить объект {post_id}')
+    post_id = None
+    try:
+        post_id = post()
+        yield post_id
+    finally:
+        if post_id:
+            delete(post_id)
 
 
 @pytest.mark.critical
@@ -69,11 +82,7 @@ def test_post(name):
     obj_id = response.json()['id']
     print('Тест создания объекта пройден.')
 
-    delete_resp = requests.delete(f'{base_url}object/{obj_id}')
-    if delete_resp.status_code == 200:
-        print(f'Oбъект {obj_id} удален')
-    if delete_resp.status_code != 200:
-        print(f'Warning: не удалось удалить объект {obj_id}')
+    delete(obj_id)
 
 
 @pytest.mark.medium
@@ -112,7 +121,7 @@ def test_patch(post_and_delete):
     assert response.status_code == 200
     print(f'Частично обновили на: {response.text}')
 
-
+@pytest.mark.medium
 def test_put(post_and_delete):
     update_body = {
         'name': fake.name(),
@@ -126,3 +135,15 @@ def test_put(post_and_delete):
     response = requests.put(put_url, json=update_body)
     assert response.status_code == 200
     print(f'Обновили на: {response.text}')
+
+
+@pytest.mark.medium
+def test_check_delete():
+    post_id = post()
+    delete(post_id)
+    delete_url = f'{base_url}object/{post_id}'
+    response = requests.delete(delete_url)
+    if response.status_code == 404:
+        print(f'При повторной попытке удаления объект {post_id} не найден в БД')
+    else:
+        print(f'Warning: объект {post_id} найден в БД')
