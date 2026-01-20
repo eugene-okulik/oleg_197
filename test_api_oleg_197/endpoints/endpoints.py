5fb98973e913a5d6df24fc847a4de248d7396b24
Oleg Vargin <oleg_197@bk.ru>
2026-01-20 22:50:37 +0300

    как же без замечаний по flake8

--- test_api_oleg_197/endpoints/endpoints.py
+++ test_api_oleg_197/endpoints/endpoints.py
@@ -8,7 +8,8 @@ class BaseAsserts:
 
     @staticmethod
     def assert_status_code(response, expected_status=200):
-        assert response.status_code == expected_status, f'Ожидался статус {expected_status}, получен: {response.status_code}'
+        assert response.status_code == expected_status, \
+            f'Ожидался статус {expected_status}, получен: {response.status_code}'
 
     @staticmethod
     def assert_update(response, expected_name=None, expected_data=None):
